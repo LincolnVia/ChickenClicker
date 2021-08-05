6580d2cb6137df3d2013a05ff8e059b8c47dc1e3
EvaZeVolt <71860413+EvaZeVolt@users.noreply.github.com>
2021-08-05 11:39:55 -0400

    Update Index.js

--- Js/Index.js
+++ Js/Index.js
@@ -1,6 +1,6 @@
 var eggs = 1;
 var autoclick = 0;
-var Quack = new Audio("Quack.mp3");
+var Quack = new Audio("SoundEffects/Quack.mp3");
 
 function EggClicker() {
   eggs = eggs + 1;
@@ -9,9 +9,24 @@ function EggClicker() {
 function update() {
   document.getElementById("AmountEgg").value = eggs;
   if (eggs == 100) {
-   Quack.loop = false;
+    Quack.loop = false;
     Quack.play();
-    
+    eggs = 101;
+  }
+  if (eggs == 101) {
+    Quack.loop = false;
+    Quack.play();
+    eggs = 102;
+  }
+  if (eggs == 102) {
+    Quack.loop = false;
+    Quack.play();
+    eggs = 103;
+  }
+  if (eggs == 103) {
+    Quack.loop = false;
+    Quack.play();
+    eggs = 104;
   }
 }
 function timer() {
