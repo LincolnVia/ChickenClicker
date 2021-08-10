606c9354b6fa00f2f02886e08a4c5d34f28b7610
EvaZeVolt <71860413+EvaZeVolt@users.noreply.github.com>
2021-08-10 09:01:31 -0400

    Update Index.js

--- Js/Index.js
+++ Js/Index.js
@@ -109,7 +109,7 @@ function buyFork() {
   }
 }
 function PrestigeUp() {
-  if (eggs >= (Prestige + 1) * 100000) {
+  if (eggs >= (Prestige + 1) * 20000) {
     eggs = eggs - eggs;
     Prestige = Prestige + 1;
 
